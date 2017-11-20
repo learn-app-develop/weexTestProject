1bd53e412f5e54996eb0139d60bf7a63969cacf5
heng.wu <heng.wu@ymm56.com>
2017-11-20 21:47:29 +0800

    Initial Commit

---
+++ weexTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weexTestProject
+//
+//  Created by heng wu on 17/11/20.
+//  Copyright © 2017年 heng wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weexTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weexTestProject
+//
+//  Created by heng wu on 17/11/20.
+//  Copyright © 2017年 heng wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
